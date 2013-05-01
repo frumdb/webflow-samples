fb7b1e9d9d0ac78335b92f00a47ccdf02e52e00f
Farshad Rum <frum@dandb.com>
2013-05-01 08:19:46 -0700

    finished equality for all, Redux ch

--- money-kent-beck/src/main/java/Dollar.java
+++ money-kent-beck/src/main/java/Dollar.java
@@ -1,7 +1,5 @@
 
-public class Dollar {
-	
-	private int amount;
+public class Dollar extends Money {
 	
 	Dollar(int amount) {
 		this.amount = amount;
@@ -11,9 +9,6 @@ public class Dollar {
 		return new Dollar(amount*multiplier);
 	}
 	
-	public boolean equals(Object object){
-		Dollar dollar = (Dollar) object;
-		return amount == dollar.amount;
-	}
+
 
 }
--- money-kent-beck/src/main/java/Franc.java
+++ money-kent-beck/src/main/java/Franc.java
@@ -1,7 +1,5 @@
 
-public class Franc {
-	
-	private int amount;
+public class Franc extends Money {
 	
 	Franc(int amount) {
 		this.amount = amount;
@@ -11,9 +9,4 @@ public class Franc {
 		return new Franc(amount*multiplier);
 	}
 	
-	public boolean equals(Object object){
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
-	}
-
 }
